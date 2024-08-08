c887053eef937102944b475f270321c3695921ef
M1zir <abubakra7mad>
2024-08-08 19:43:06 +0400

    style: adding animations

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Register the ScrollTrigger plugin
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, MotionPathPlugin);
 let smoother = ScrollSmoother.create({
   wrapper: "#smooth-wrapper",
   content: "#smooth-content",
@@ -26,19 +26,5 @@ paths.forEach((path) => {
   });
 });
 
-//register the plugin (just once)
-gsap.registerPlugin(MotionPathPlugin);
 
-gsap.to("#Lines", {
-  duration: 5,
-  repeat: 12,
-  repeatDelay: 3,
-  yoyo: true,
-  ease: "power1.inOut",
-  motionPath: {
-    path: "M17.27,-404.545 C19.088,-193.533 20.801,-59.987 22.725,-0.001 ",
-    align: "#path",
-    autoRotate: true,
-    alignOrigin: [0.5, 0.5]
-  }
-});
+
